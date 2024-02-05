8de182c72c1d7b05ff3f718aa11242121089d391
Jake Haitsma <jacobhaitsma@gmail.com>
2024-02-04 20:52:57 -0500

    chore(dev): add example app content.

--- examples/index.ts
+++ examples/index.ts
@@ -1,22 +1,45 @@
 import * as PIXI from "pixi.js";
 import { BitmapTextInput } from "../src/index";
 
-function init() {
+async function init() {
+  await PIXI.Assets.load("./fonts/Foxley816/bitmap/Foxley816.fnt");
+
   const canvas = document.createElement("canvas");
   document.body.appendChild(canvas);
 
   const app = new PIXI.Application({
-    width: 800,
-    height: 600,
-    backgroundColor: "#000000",
+    backgroundColor: "#02040a",
     antialias: true,
     resolution: 1,
     view: canvas,
   });
 
-  const bitmapTextInput = new BitmapTextInput();
-  bitmapTextInput.x = 100;
-  bitmapTextInput.y = 100;
+  const bitmapTextInput = new BitmapTextInput("Lorem ipsum dolor sit amet.", {
+    fontName: "Foxley816",
+  });
+  bitmapTextInput.x = Math.round(150);
+  bitmapTextInput.y = Math.round(150);
+
+  let dx = 1;
+  let dy = 1;
+  app.ticker.add(() => {
+    bitmapTextInput.x += dx;
+    bitmapTextInput.y += dy;
+
+    if (
+      bitmapTextInput.x < 0 ||
+      bitmapTextInput.x > app.screen.width - bitmapTextInput.width
+    ) {
+      dx *= -1;
+    }
+
+    if (
+      bitmapTextInput.y < 0 ||
+      bitmapTextInput.y > app.screen.height - bitmapTextInput.height
+    ) {
+      dy *= -1;
+    }
+  });
 
   app.stage.addChild(bitmapTextInput);
 }
